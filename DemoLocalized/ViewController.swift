b2ababdde960c85bcc9aa0f8ffcaa497ba554129
Dung <dungkv@Dungs-MacBook-Pro.local>
2018-08-18 12:47:03 +0700

    localized app

--- DemoLocalized/ViewController.swift
+++ DemoLocalized/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
+    @IBOutlet private weak var lbContent: UILabel!
+    @IBOutlet private weak var btnOpenGallery: UIButton!
+    
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from
+        lbContent.text = "Content".localized;
+        btnOpenGallery.setTitle("Camera".localized, for: .normal)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction private func btnOpenGallery(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+}
 
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: "")
+    }
 }
 
